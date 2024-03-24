958c08650bc36974e417558c347dabf14bac7d7a
Ketan R <ketan_r598@outlook.com>
2024-03-24 14:30:17 +0530

    updated .gitignore

---
+++ src/main/java/SpringLoginAndRegistrationWithTokens/SpringLoginAndRegistrationWithTokensApplication.java
@@ -0,0 +1,13 @@
+package SpringLoginAndRegistrationWithTokens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginAndRegistrationWithTokensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginAndRegistrationWithTokensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringLoginAndRegistrationWithTokens/SpringLoginAndRegistrationWithTokensApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringLoginAndRegistrationWithTokens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginAndRegistrationWithTokensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
